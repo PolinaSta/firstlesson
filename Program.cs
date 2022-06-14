246b46f4c22283da60910285dbb6d51a0731b807
Polina <polstaver@yandex.by>
2022-06-14 18:03:54 +0300

    скопированы данные с семнара

--- Program.cs
+++ Program.cs
@@ -1,1 +1,61 @@
-﻿jhvkhvkv,nb
+﻿// Первая задача
+// Console.WriteLine("input integer number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num * num;
+// Console.WriteLine("result is" + result);
+
+// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// Console.WriteLine("input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 == num2*num2){
+//     Console.WriteLine("first number is a square of second number:");
+// }
+// else {
+//     Console.WriteLine("first number is not a square of second number:");
+// }
+
+// Задача 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// Console.WriteLine("input number of a day: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day < 1 || day > 7){
+//     Console.WriteLine("incorrect day");
+// }
+// else {
+//     if (day == 1){
+//        Console.WriteLine("monday"); 
+//     }
+//     if (day == 2){
+//        Console.WriteLine("tuesday"); 
+//     }
+//     if (day == 3){
+//        Console.WriteLine("wensday"); 
+//     }
+// 
+
+// 4. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+ 
+//  Console.WriteLine("input N: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int current;
+// if (num < 0){
+//     current = num;
+//     num = num * (-1);
+// }
+// else {
+//     current = -1 * num;
+// }
+// while (current < num-1){
+//     current++;
+//     Console.Write(current + " ");
+// }
+
+// 5. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// Console.WriteLine("input N: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(num % 10);
+
+
+
